a3333413af95a0791b2230ba113fdd20fb09bca3
Dmitry Sulman <dmitry.sulman@gmail.com>
2023-03-02 19:56:49 +0200

    HW36

---
+++ src/telran/io/test/InputOutputTest.java
@@ -0,0 +1,77 @@
+package telran.io.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class InputOutputTest {
+
+	private static final int MARGIN_LENGTH = 3;
+
+	private String fileName = "myFile";
+	private String directoryName = "myDirectory1/myDirectory2";
+
+	@AfterEach
+	void tearDown() throws Exception {
+		new File(fileName).delete();
+		new File(directoryName).delete();
+	}
+
+	@Test
+	@Disabled
+	void test() throws IOException {
+		File f1 = new File(fileName);
+		assertTrue(f1.createNewFile());
+
+		File dir1 = new File(directoryName);
+		assertTrue(dir1.mkdirs());
+		System.out.println(dir1.getAbsolutePath());
+	}
+
+	@Test
+	void printDirectoryFilesTest() throws IOException {
+		printDirectoryFiles(".", 0);
+	}
+
+	void printDirectoryFiles(String path, int maxLevel) throws IOException {
+		if (maxLevel < 1) {
+			maxLevel = Integer.MAX_VALUE;
+		}
+		Path dir = Path.of(path).toAbsolutePath().normalize();
+		Files.walkFileTree(dir, Collections.emptySet(), maxLevel, new PrintFileVisitor(dir));
+	}
+
+	@Test
+	void printDirectoryFileTest() throws IOException {
+		printDirectoryFile(".", 0);
+	}
+
+	void printDirectoryFile(String path, int maxLevel) throws IOException {
+		if (maxLevel < 1) {
+			maxLevel = Integer.MAX_VALUE;
+		}
+		File dir = new File(path).getCanonicalFile();
+		printDirectoryFile(dir, maxLevel, 0);
+	}
+
+	void printDirectoryFile(File currentFile, int maxLevel, int currentLevel) {
+		if (currentLevel <= maxLevel) {
+			String type = currentFile.isDirectory() ? "dir" : "file";
+			System.out.printf("%s%s - %s\n", " ".repeat(currentLevel * MARGIN_LENGTH), currentFile.getName(), type);
+			if (currentFile.isDirectory()) {
+				File[] files = currentFile.listFiles();
+				Arrays.stream(files).forEach(file -> printDirectoryFile(file, maxLevel, currentLevel + 1));
+			}
+		}
+	}
+
+}
---
+++ src/telran/io/test/PrintFileVisitor.java
@@ -0,0 +1,35 @@
+package telran.io.test;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class PrintFileVisitor extends SimpleFileVisitor<Path> {
+	private final Path rootDir;
+	private static final int MARGIN_LENGTH = 3;
+
+	public PrintFileVisitor(Path rootDir) {
+		this.rootDir = rootDir;
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+		printNode(dir);
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		printNode(file);
+		return FileVisitResult.CONTINUE;
+	}
+
+	private void printNode(Path node) {
+		int margin = (node.getNameCount() - rootDir.getNameCount()) * MARGIN_LENGTH;
+		String type = Files.isDirectory(node) ? "dir" : "file";
+		System.out.printf("%s%s - %s\n", " ".repeat(margin), node.getFileName(), type);
+	}
+}
